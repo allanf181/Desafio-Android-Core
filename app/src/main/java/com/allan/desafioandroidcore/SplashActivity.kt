2d849d423e1dcf95d13a560e31ae9dfa1104ed9a
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2020-11-08 19:24:35 -0300

    Splash screen

---
+++ app/src/main/java/com/allan/desafioandroidcore/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.allan.desafioandroidcore
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val i = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 2500)
+    }
+}
